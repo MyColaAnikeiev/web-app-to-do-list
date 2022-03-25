693c80343b9027ffb99a345046d6ba38b5be0381
MyColaAnikeiev <mykolaanikeiev@gmail.com>
2022-03-25 15:15:45 +0200

    Added switching logic. Day switching buttons will consist of current and from current +-3 days (7 total).

--- src/app/switcher/switcher/switcher.component.ts
+++ src/app/switcher/switcher/switcher.component.ts
@@ -1,15 +1,45 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+
+
+type buttonValue = {day: string, date: string};
+
 
 @Component({
   selector: '.app-switcher',
   templateUrl: './switcher.component.html',
   styleUrls: ['./switcher.component.scss']
 })
-export class SwitcherComponent implements OnInit {
+export class SwitcherComponent implements OnInit {  
+  public buttonValues: buttonValue[] = [];
+  public currentDay = 'Mon';
 
   constructor() { }
 
   ngOnInit(): void {
+    this.currentDay = (new Date()).toDateString().slice(0,3);
+
+    this.buttonValues = this.genButtons();
+  }
+
+  genButtons() : buttonValue[] {
+    const date = new Date();
+    date.setDate(date.getDate()-3);
+
+    const days = [];
+
+    for(let i = 0; i < 7; i++){
+      const dateString = formatDate(date, 'dd-MM','en-US');
+      const dayName = date.toDateString().slice(0,3);
+      date.setDate(date.getDate()+1);
+      days.push({ day: dayName, date: dateString });
+    }
+
+    return days;
+  }
+
+  selectDay(index: number){
+    this.currentDay = this.buttonValues[index].day;
   }
 
 }
